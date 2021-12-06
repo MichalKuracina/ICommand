46799d8ebd3e02a80a6253c6c482ff79ca692f50
Michal Kuracina <michal.kuracina@henkel.com>
2021-12-06 16:24:51 +0100

    Add project files.

---
+++ wpf_icommand/ViewModels/MessageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using wpf_icommand.ViewModels.Commands;
+
+namespace wpf_icommand.ViewModels
+{
+    public class MessageViewModel
+    {
+        public string MessageText { get; set; }
+        public MessageCommand DisplayMessageCommand { get; private set; }
+
+        public MessageViewModel()
+        {
+            DisplayMessageCommand = new MessageCommand(DisplayMessage);
+        }
+
+        public void DisplayMessage()
+        {
+            MessageBox.Show(MessageText);
+        }
+    }
+}
